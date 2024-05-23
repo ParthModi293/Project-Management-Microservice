f37bd362e7c6b49dcb1a8eb578cded1c22733a87
ParthModi293 <parthrmodi17@gmail.com>
2024-05-23 18:40:52 +0530

    all changed

--- Task-Submission-Service/src/main/java/com/parth/service/TaskService.java
+++ Task-Submission-Service/src/main/java/com/parth/service/TaskService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(url = "${TASK-SERVICE.url}", name = "TASK-SERVICE")
+@FeignClient(url = "${task_service.url}", name = "TASK-SERVICE")
 public interface TaskService {
 
     @GetMapping("/api/tasks/{id}")
--- Task-Submission-Service/src/main/java/com/parth/service/UserService.java
+++ Task-Submission-Service/src/main/java/com/parth/service/UserService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "USER-SERVICE" , url = "${USER-SERVICE.url}")
+@FeignClient(name = "USER-SERVICE" , url = "${user_service.url}")
 public interface UserService {
 
     @GetMapping("/api/users/profile")
